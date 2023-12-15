7e007331a22f799f217cd988ed18da7d0eb21c49
Wren <OtherwiseJunk@gmail.com>
2023-12-15 16:15:26 -0500

    cleanup layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,43 @@ import GameBoard from "../components/gameBoard";
 
 export default function Home() {
   return (
-    <div className="logo bordered container bg-amber-900 flex justify-content-center align-items-center">
+    <div className="h-max w-max logo bordered container text-xl bg-yellow-950 p-5">
       <div className="grid grid-rows-4 grid-cols-5 text-center">
-        <div className="p-10">
+        <div className="p-5 m-auto">
           <Image
             className="logo bordered"
             alt="A blue-skinned character deep in thought."
             src="/logo.png"
-            width={300}
-            height={300}
+            width={128}
+            height={128}
           />
         </div>
-        <div className="text-4xl m-auto">Sorcery</div>
-        <div className="text-4xl m-auto">Enchantments</div>
-        <div className="p-10">
+        <div className="m-auto">Sorcery</div>
+        <div className="m-auto">Enchantments</div>
+        <div className="p-5 m-auto">
           <Image
             className="logo bordered top-left top-right bottom-left bottom-right"
             alt="A red mana symbol; a poorly drawn fireball sillouhete on a red field."
             src="/mountain.png"
-            width={300}
-            height={300}
+            width={128}
+            height={128}
           />
         </div>
         <div className=""></div>
-        <div className="p-10">
+        <div className="p-5 m-auto">
           <Image
             className="logo bordered top-left top-right bottom-left bottom-right"
             alt="A green mana symbol; a poorly drawn tree sillouhete on a green field."
             src="/forest.png"
-            width={300}
-            height={300}
+            width={128}
+            height={128}
           />
         </div>
         <GameBoard />
         <div className=""></div>
-        <div className="text-4xl m-auto">Ravnica: City of Guilds</div>
-        <div className="text-4xl m-auto">Life Points: 0</div>
-        <div className="text-4xl m-auto">Goblin</div>
+        <div className="p-5 m-auto">Ravnica: City of Guilds</div>
+        <div className="m-auto">Life Points: 9</div>
+        <div className="p-5 m-auto">Goblin</div>
         <div className=""></div>
       </div>
       <div className="paperOverlay"></div>
--- src/components/gameBoard.tsx
+++ src/components/gameBoard.tsx
@@ -1,39 +1,43 @@
 import Image from "next/image";
 
+function getColorClassByIndex(index: number) {
+  let colorClass = "";
+  switch (index) {
+    case 0:
+      colorClass = "bg-yellow-300";
+      break;
+    case 2:
+      colorClass = "bg-blue-600";
+      break;
+    case 4:
+      colorClass = "bg-gray-600";
+      break;
+    case 6:
+      colorClass = "bg-red-600";
+      break;
+    case 8:
+      colorClass = "bg-green-600";
+      break;
+    default:
+      colorClass = "bg-amber-600";
+      break;
+  }
+  return colorClass;
+}
+
 export default function GameBoard() {
+  const numberOfSquares = 9;
   return (
-    <div className="game-input-board bordered grid col-span-3 row-span-3 grid-cols-3 grid-rows-3 text-4xl">
-      <div className="game-border top-left bg-yellow-300  px-12 py-4">
-        Primal Command
-      </div>
-      <div className="bg-amber-900 game-submitted game-border px-12 py-4">
-        <Image
-          className="logo bordered top-left top-right bottom-left bottom-right"
-          src="https://cards.scryfall.io/large/front/8/6/86d6b411-4a31-4bfc-8dd6-e19f553bb29b.jpg"
-          alt="Rancor MTG"
-          width={190}
-          height={210}
-        />
-      </div>
-      <div className="game-border top-right bg-blue-400 px-12 py-4">
-        Atraka's Command
-      </div>
-      <div className="bg-amber-900 game-border px-12 py-4">
-        Hour of Reckoning
-      </div>
-      <div className="bg-amber-900 game-border bg-gray-400  px-12 py-4">
-        Concerted Effort
-      </div>
-      <div className="bg-amber-900 game-border  px-12 py-4">Rain of Embers</div>
-      <div className="game-border bottom-left bg-red-400 px-12 py-4">
-        Boggart Birth Rite
-      </div>
-      <div className="bg-amber-900 game-border  px-12 py-4">
-        Boggart Shenanigans
-      </div>
-      <div className="game-border bottom-right bg-green-400 px-12 py-4">
-        Goblin Ravenger
-      </div>
+    <div className="game-input-board bordered grid col-span-3 row-span-3 grid-cols-3 grid-rows-3 text-2xl">
+      <div className="bg-amber-700 game-border px-12 py-4 top-left"></div>
+      <div className="bg-amber-700 game-border px-12 py-4"></div>
+      <div className="bg-amber-700 game-border px-12 py-4 top-right"></div>
+      <div className="bg-amber-700 game-border px-12 py-4"></div>
+      <div className="bg-amber-700 game-border px-12 py-4 input-center bg-auto"></div>
+      <div className="bg-amber-700 game-border px-12 py-4"></div>
+      <div className="bg-amber-700 game-border px-12 py-4 bottom-left"></div>
+      <div className="bg-amber-700 game-border px-12 py-4"></div>
+      <div className="bg-amber-700 game-border px-12 py-4 bottom-right"></div>
     </div>
   );
 }
