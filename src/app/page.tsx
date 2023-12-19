0c0995f2367ae086a02d2000c75887db96366909
Wren <OtherwiseJunk@gmail.com>
2023-12-19 01:43:20 -0500

    style: lint autofixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-
 import Game from "@/components/game";
-import { GameConstraint } from "./models/headerSquareInfo";
+import { GameConstraint } from "./models/UI/gameConstraint";
 
 export default function Home() {
-
-  return (
-    <Game/>
-  );
+  return <Game />;
 }
