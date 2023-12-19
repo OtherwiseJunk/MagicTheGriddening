5e57d88394cb7d8f2f9dbf9e7dc64f0d9a6773c9
Wren <OtherwiseJunk@gmail.com>
2023-12-19 14:04:35 -0500

    chore: move models to src root

--- src/models/UI/gameConstraint.ts
+++ src/models/UI/gameConstraint.ts
@@ -1,7 +1,15 @@
 export class GameConstraint{
     constructor(
         public displayName: string,
+        public constraintType: ConstraintType,
         public imageSrc: string ="",
         public imageAltText: string = ""
     ){}
+}
+
+export enum ConstraintType {
+    Rarity,
+    Type,
+    Color,
+    Set,
 }
