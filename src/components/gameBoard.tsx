f0599ed6a934af54c338dca84e83f0af075dd18c
Wren <OtherwiseJunk@gmail.com>
2023-12-18 22:20:23 -0500

    add colorless image. update plains to match real color. clean-up gameBoard

--- src/components/gameBoard.tsx
+++ src/components/gameBoard.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 
 export default function GameBoard() {
   return (
-    <div className="game-input-board bordered grid col-span-3 row-span-3 grid-cols-3 grid-rows-3 text-2xl">
+    <div className="grid col-span-3 row-span-3 grid-cols-3 grid-rows-3 text-2xl">
       <div className="bg-amber-700 game-border px-12 py-4 top-left"></div>
       <div className="bg-amber-700 game-border px-12 py-4"></div>
       <div className="bg-amber-700 game-border px-12 py-4 top-right"></div>
