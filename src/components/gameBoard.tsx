16b2fbef544682c0db257aef30afab694d0faffc
Wren <OtherwiseJunk@gmail.com>
2023-12-18 23:54:13 -0500

    style: reduce horizontal padding of gameBoard squares

--- src/components/gameBoard.tsx
+++ src/components/gameBoard.tsx
@@ -3,15 +3,15 @@
 export default function GameBoard() {
   return (
     <div className="grid col-span-3 row-span-3 grid-cols-3 grid-rows-3 text-2xl">
-      <div className="bg-amber-700 game-border px-12 py-4 top-left"></div>
-      <div className="bg-amber-700 game-border px-12 py-4"></div>
-      <div className="bg-amber-700 game-border px-12 py-4 top-right"></div>
-      <div className="bg-amber-700 game-border px-12 py-4"></div>
-      <div className="bg-amber-700 game-border px-12 py-4 input-center bg-auto"></div>
-      <div className="bg-amber-700 game-border px-12 py-4"></div>
-      <div className="bg-amber-700 game-border px-12 py-4 bottom-left"></div>
-      <div className="bg-amber-700 game-border px-12 py-4"></div>
-      <div className="bg-amber-700 game-border px-12 py-4 bottom-right"></div>
+      <div className="bg-amber-700 game-border px-10 py-4 top-left"></div>
+      <div className="bg-amber-700 game-border px-10 py-4"></div>
+      <div className="bg-amber-700 game-border px-10 py-4 top-right"></div>
+      <div className="bg-amber-700 game-border px-10 py-4"></div>
+      <div className="bg-amber-700 game-border px-10 py-4 input-center bg-auto"></div>
+      <div className="bg-amber-700 game-border px-10 py-4"></div>
+      <div className="bg-amber-700 game-border px-10 py-4 bottom-left"></div>
+      <div className="bg-amber-700 game-border px-10 py-4"></div>
+      <div className="bg-amber-700 game-border px-10 py-4 bottom-right"></div>
     </div>
   );
 }
