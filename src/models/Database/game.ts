bd0b6b24f8006e64aceacb08684b66149516a89f
Wren <OtherwiseJunk@gmail.com>
2023-12-21 19:28:11 -0500

    feat: Add function for translating dateString to Date

--- src/models/Database/game.ts
+++ src/models/Database/game.ts
@@ -1,9 +1,16 @@
 import { GameConstraint } from '../UI/gameConstraint';
 
 export class Game {
-    constructor(public id: number, public day: string, public constraintsJSON: string) { }
+    constructor(public id: number, public dateString: string, public constraintsJSON: string) { }
 
     toUIObject(): GameConstraint[] {
         return JSON.parse(this.constraintsJSON);
+    }
+
+    dateStringToDate(): Date{
+        const year = parseInt(this.dateString.substring(0,4));
+        const month = parseInt(this.dateString.substring(4,6));
+        const day = parseInt(this.dateString.substring(6));
+        return new Date(`${month+1}/${day}/${year}`)
     }
 }
