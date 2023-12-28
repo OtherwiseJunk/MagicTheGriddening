422330c2df88d3fab04c2d03509f8e482b1dff4c
Wren <OtherwiseJunk@gmail.com>
2023-12-28 16:54:38 -0500

    feat: Add Dialog to gameBoard for submitting card answers Send additional props to InputSquare for hndling dialog launches

--- src/components/gameBoard.tsx
+++ src/components/gameBoard.tsx
@@ -1,27 +1,110 @@
 "use client";
 
+import { GameState } from "@/models/UI/gameState";
 import InputSquare from "./inputSquare";
+import { useState } from "react";
+import InputDialog from "./inputDialog";
 
-export default function GameBoard() {
-  const inputSquares = Array.from({ length: 9 }, (value, index) => index).map((_, index)=>{
-    switch(index){
+type GameBoardPrompts = {
+  gameState: GameState;
+  setGameState: Function;
+};
+
+const inputSquares = (
+  props: GameBoardPrompts,
+  handleOpen: Function,
+  setDialogIndex: Function
+) => {
+  return Array.from({ length: 9 }, (value, index) => index).map((_, index) => {
+    const correctGuess = props.gameState.correctGuesses.find(
+      (correctGuess) => correctGuess.squareIndex === index
+    );
+    switch (index) {
       case 0:
-        return <InputSquare topLeftCorner={true} />
+        return (
+          <InputSquare
+            key={index}
+            topLeftCorner={true}
+            correctGuess={correctGuess}
+            handleOpen={handleOpen}
+            gridIndex={index}
+            setDialogIndex={setDialogIndex}
+          />
+        );
       case 2:
-        return <InputSquare topRightCorner={true} />
+        return (
+          <InputSquare
+            key={index}
+            topRightCorner={true}
+            correctGuess={correctGuess}
+            handleOpen={handleOpen}
+            gridIndex={index}
+            setDialogIndex={setDialogIndex}
+          />
+        );
       case 4:
-        return <InputSquare center={true}/>
+        return (
+          <InputSquare
+            key={index}
+            center={true}
+            correctGuess={correctGuess}
+            handleOpen={handleOpen}
+            gridIndex={index}
+            setDialogIndex={setDialogIndex}
+          />
+        );
       case 6:
-        return <InputSquare bottomLeftCorner={true} />
+        return (
+          <InputSquare
+            key={index}
+            bottomLeftCorner={true}
+            correctGuess={correctGuess}
+            handleOpen={handleOpen}
+            gridIndex={index}
+            setDialogIndex={setDialogIndex}
+          />
+        );
       case 8:
-        return <InputSquare bottomRightCorner={true} />
+        return (
+          <InputSquare
+            key={index}
+            bottomRightCorner={true}
+            correctGuess={correctGuess}
+            handleOpen={handleOpen}
+            gridIndex={index}
+            setDialogIndex={setDialogIndex}
+          />
+        );
       default:
-        return <InputSquare/>
+        return (
+          <InputSquare
+            key={index}
+            correctGuess={correctGuess}
+            handleOpen={handleOpen}
+            gridIndex={index}
+            setDialogIndex={setDialogIndex}
+          />
+        );
     }
-  })
+  });
+};
+
+export default function GameBoard(props: GameBoardPrompts) {
+  const [isOpen, setOpen] = useState(false);
+  const [dialogGridIndex, setDialogGridIndex] = useState(-1);
+  const handleOpen = () => setOpen(true);
+  
+
   return (
     <div className="grid col-span-3 row-span-3 grid-cols-3 grid-rows-3">
-      {inputSquares}
+      {inputSquares(props, handleOpen, setDialogGridIndex)}
+      {InputDialog({
+        isOpen: isOpen,
+        setIsOpen: setOpen,
+        dialogGridIndex: dialogGridIndex,
+        gameState: props.gameState,
+        setGameState: props.setGameState,
+      })}
     </div>
   );
 }
