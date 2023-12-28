4bce180da82fe5038dbb10ea6d69cf5598ca7322
Wren <OtherwiseJunk@gmail.com>
2023-12-28 16:49:39 -0500

    feat: Add logic to inputSquare for handling correct guesses and onClick

--- src/components/inputSquare.tsx
+++ src/components/inputSquare.tsx
@@ -1,39 +1,46 @@
-class InputSquareProps{
-    topLeftCorner?: boolean;
-    topRightCorner?: boolean;
-    bottomLeftCorner?: boolean;
-    bottomRightCorner?: boolean;
-    center?: boolean
+import { CorrectGuess } from "@/models/UI/correctGuess";
+
+class InputSquareProps {
+  topLeftCorner?: boolean = false;
+  topRightCorner?: boolean = false;
+  bottomLeftCorner?: boolean = false;
+  bottomRightCorner?: boolean = false;
+  center?: boolean = false;
+  correctGuess?: CorrectGuess;
+  handleOpen: Function = () =>{};
+  gridIndex: number = -1;
+  setDialogIndex: Function = () =>{};
 }
 
-const defaultProps: InputSquareProps = {
-    topLeftCorner: false,
-    topRightCorner: false,
-    bottomLeftCorner: false,
-    bottomRightCorner: false,
-    center: false
+function getPositionalStyleClass(props: InputSquareProps) {
+  let styleClass = "paper-texture bg-amber-700 square game-border px-4";
+  if (props.bottomLeftCorner) styleClass += " bottom-left";
+  if (props.bottomRightCorner) styleClass += " bottom-right";
+  if (props.topLeftCorner) styleClass += " top-left";
+  if (props.topRightCorner) styleClass += " top-right";
+  if (props.center) {
+    styleClass = styleClass.replace("paper-texture", "").trim();
+    styleClass += " input-center bg-auto";
+  }
+  return styleClass;
 }
 
-function getPositionalStyleClass(props: InputSquareProps){
-    let styleClass = "paper-texture bg-amber-700 square game-border px-10 py-4";
-    if(props.bottomLeftCorner) styleClass += " bottom-left";
-    if(props.bottomRightCorner) styleClass += " bottom-right";
-    if(props.topLeftCorner) styleClass += " top-left";
-    if(props.topRightCorner) styleClass += " top-right";
-    if(props.center){
-        styleClass = styleClass.replace('paper-texture', '').trim();
-        styleClass += " input-center bg-auto"
-    } 
-    return styleClass
-}
+export default function InputSquare(props: InputSquareProps) {
+  const positionalStyle = getPositionalStyleClass(props);
 
-const InputSquare = (props: InputSquareProps) => {
-    const positionalStyle = getPositionalStyleClass(props);
+
+  if (props.correctGuess)
     return (
-        <div className={positionalStyle}></div>
-    )
+      <div className={positionalStyle}>
+        <img
+          className="correct-answer"
+          alt={props.correctGuess.cardName}
+          src={props.correctGuess.imageUrl}
+        />
+      </div>
+    );
+  return <div className={positionalStyle} onClick={() => {
+    props.setDialogIndex(props.gridIndex);
+    props.handleOpen()
+  }}></div>;
 }
-
-InputSquare.defaultProps = defaultProps;
-
-export default InputSquare;
