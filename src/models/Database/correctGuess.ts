f0ffe958da40d142cd232920dce9c2f1b5b1c745
Wren <OtherwiseJunk@gmail.com>
2023-12-29 13:58:06 -0500

    chore: lint spacing fixes

--- src/models/Database/correctGuess.ts
+++ src/models/Database/correctGuess.ts
@@ -1,11 +1,18 @@
 export class CorrectGuess {
-    constructor(public id: number, public playerRecordId: number, public gameId: number, public correctGuess: string, public squareIndex: number, public imageSource: string) { }
+  constructor(
+    public id: number,
+    public playerRecordId: number,
+    public gameId: number,
+    public correctGuess: string,
+    public squareIndex: number,
+    public imageSource: string
+  ) {}
 
-    toUIObject() {
-        return {
-            cardName: this.correctGuess,
-            imageUrl: this.imageSource,
-            squareIndex: this.squareIndex
-        }
-    }
+  toUIObject() {
+    return {
+      cardName: this.correctGuess,
+      imageUrl: this.imageSource,
+      squareIndex: this.squareIndex,
+    };
+  }
 }
