9dd27ac6881f150e2b05ed2e674622d27b758e38
Wren <OtherwiseJunk@gmail.com>
2023-12-29 16:19:52 -0500

    chore: prettier

--- src/app/api/gameState/[playerId]/route.ts
+++ src/app/api/gameState/[playerId]/route.ts
@@ -7,14 +7,15 @@ export async function GET(
   request: Request,
   { params }: { params: { playerId: string } }
 ) {
-  const gameState = await buildGameStateForUser(params.playerId);  
-  if(gameState.gameConstraints.length){
-    return new Response(
-        JSON.stringify(gameState)
-      );
+  const gameState = await buildGameStateForUser(params.playerId);
+  if (gameState.gameConstraints.length) {
+    return new Response(JSON.stringify(gameState));
   }
 
-  return new Response(null, {status: 500, statusText: "Failed to retrieve today's game :-("});  
+  return new Response(null, {
+    status: 500,
+    statusText: "Failed to retrieve today's game :-(",
+  });
 }
 
 async function buildGameStateForUser(playerId: string): Promise<GameState> {
@@ -30,8 +31,8 @@ async function buildGameStateForUser(playerId: string): Promise<GameState> {
 async function getGameData(): Promise<[GameConstraint[], number]> {
   let game = await DataService.getTodaysGame();
   if (!game) {
-    console.log('Ran into an issue getting today\'s game :(');
-    return [[], 0]
+    console.log("Ran into an issue getting today's game :(");
+    return [[], 0];
   }
 
   return [game.toUIObject(), game!.id];
@@ -40,15 +41,15 @@ async function getGameData(): Promise<[GameConstraint[], number]> {
 async function getUserDataForGame(
   playerId: string,
   gameId: number
-): Promise<
-  [number, CorrectGuess[]]
-> {
-  const [lifePoints, correctGuesses] =  await DataService.getPlayerGameData(gameId, playerId);
+): Promise<[number, CorrectGuess[]]> {
+  const [lifePoints, correctGuesses] = await DataService.getPlayerGameData(
+    gameId,
+    playerId
+  );
 
   if (lifePoints !== -1) {
-    return [lifePoints, correctGuesses];  
+    return [lifePoints, correctGuesses];
   }
 
-  return [9, correctGuesses]
+  return [9, correctGuesses];
 }
-
