fbeaf738f06503f30ec67ca065a21819067adcc5
Wren <OtherwiseJunk@gmail.com>
2023-12-29 13:57:24 -0500

    feat: add logic for generating type gameConstraints in constructor

--- src/models/UI/gameConstraint.ts
+++ src/models/UI/gameConstraint.ts
@@ -1,18 +1,3 @@
-const colorToColorCode = new Map<string,string>([
-    ["White","w"],
-    ["Blue","u"],
-    ["Black","b"],
-    ["Red","r"],
-    ["Green","g"],
-    ["Colorless","c"]
-]);    
-
-const rarityToRarityCode = new Map<string,string>([
-    ["Mythic", "m"],
-    ["Rare", "r"],
-    ["Uncommon", "u"],
-    ["Common", "c"]
-]);
 
 export class GameConstraint {
   constructor(
@@ -22,18 +7,8 @@ export class GameConstraint {
     public imageSrc: string = "",
     public imageAltText: string = ""
   ) {
-    if (scryfallQuery.length === 0) {
-      switch (constraintType) {
-        case ConstraintType.ManaValue:
-            this.scryfallQuery = `cmc:${parseInt(displayName.split(" ")[2])}`;
-          break;
-        case ConstraintType.Rarity:
-            this.scryfallQuery = `r:${rarityToRarityCode.get(displayName)}`;
-          break;
-        case ConstraintType.Type:
-            this.scryfallQuery = `type:${displayName}`;
-          break;
-      }
+    if (scryfallQuery.length === 0 && constraintType === ConstraintType.Type) {
+      this.scryfallQuery = `type:${displayName}`;
     }
   }
 }
