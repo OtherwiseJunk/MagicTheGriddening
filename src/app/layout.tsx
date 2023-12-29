53cbf06a20729bb25668b3d162e4386a9418a9b8
Wren <OtherwiseJunk@gmail.com>
2023-12-29 18:42:40 -0500

    lint: Manually fix lint errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,25 @@
-import type { Metadata } from 'next'
-import { Eczar } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Eczar } from "next/font/google";
+import "./globals.css";
+import React from "react";
 
-const globalFont = Eczar({ subsets: ['latin'] })
+const globalFont = Eczar({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Magic: The Griddening',
-  description: 'Game in the style of the Immaculate Grid for Magic: The Gathering cards'
+  title: "Magic: The Griddening",
+  description:
+    "Game in the style of the Immaculate Grid for Magic: The Gathering cards",
+};
 
-}
-
-export default function RootLayout ({
-  children
+export default function RootLayout({
+  children,
 }: {
-  children: React.ReactNode
-}) {
+  children: React.ReactNode;
+}): React.JSX.Element {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <body className={globalFont.className}>{children}</body>
     </html>
-  )
+  );
 }
