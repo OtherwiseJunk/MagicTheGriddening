cfc94f12b9e07c4044db86f08928435c6617ca37
Wren <OtherwiseJunk@gmail.com>
2024-01-03 19:39:47 -0500

    feat: add manifest

---
+++ src/app/manifest.tsx
@@ -0,0 +1,20 @@
+import { type MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Magic: The Griddening',
+        short_name: 'Griddening',
+        description: 'A daily puzzle game where you name magic cards meeting certain constraints',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#3A1300',
+        theme_color: '#3a3aec',
+        icons:[
+            {
+                src: '/favicon.ico',
+                sizes: 'any',
+                type: 'image/x-icon'
+            }
+        ]
+    }
+}
