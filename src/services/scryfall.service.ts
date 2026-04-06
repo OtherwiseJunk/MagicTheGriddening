8219949da6a900d3e0881990f6cc08c89180bb38
OtherwiseJunk <OtherwiseJunk@gmail.com>
2026-04-06 00:37:35 -0400

    prevent memory leak in cache by setting max cache size

--- src/services/scryfall.service.ts
+++ src/services/scryfall.service.ts
@@ -2,7 +2,8 @@
 import * as Scry from "scryfall-sdk";
 
 export default class ScryfallService {
-  static queryCountMap = new Map();
+  static queryCountMap = new Map<string, number>();
+  private static readonly MAX_CACHE_SIZE = 500;
 
   static async getCards(query: string): Promise<Scry.Card[]> {
     return await Scry.Cards.search(`${query} -is:digital`, { page: 1, unique: "cards" })
@@ -12,7 +13,10 @@ export default class ScryfallService {
 
   static async getFirstPageCardCount(query: string): Promise<number> {
     if (this.queryCountMap.has(query)) {
-      return this.queryCountMap.get(query);
+      return this.queryCountMap.get(query)!;
+    }
+    if (this.queryCountMap.size >= this.MAX_CACHE_SIZE) {
+      this.queryCountMap.clear();
     }
     const queryCardCount = (await Scry.Cards.search(query, 1).cancelAfterPage().waitForAll())
       .length;
